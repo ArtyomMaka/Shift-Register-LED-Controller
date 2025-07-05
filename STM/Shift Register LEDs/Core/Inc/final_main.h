9f3b5b5789f9a00ffa76cae2e16b27a58c0ff41f
Makarkin <91897150+ArtyomMaka@users.noreply.github.com>
2025-07-05 17:53:41 +0300

    Add true STM proj

---
+++ STM/Shift Register LEDs/Core/Inc/final_main.h
@@ -0,0 +1,15 @@
+/*
+ * final_main.h
+ *
+ *  Created on: Jul 4, 2025
+ *      Author: makar
+ */
+
+#ifndef INC_FINAL_MAIN_H_
+#define INC_FINAL_MAIN_H_
+
+#include "stm32f1xx_hal.h"
+
+void final_main(void);
+
+#endif /* INC_FINAL_MAIN_H_ */
